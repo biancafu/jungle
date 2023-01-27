4d71140cd0553f4d092f6e9a1a571ebc42d1902e
Bianca Fu <0509biancafu@gmail.com>
2023-01-26 20:20:47 -0700

    added product and category count to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_KEY']
   
   def show
+    @product_count = Order.count()
+    @category_count = Category.count()
   end
 end
