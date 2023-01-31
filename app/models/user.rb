15c1a2624643953fa41efc011ead63216e0c5609
Bianca Fu <0509biancafu@gmail.com>
2023-01-31 03:51:15 +0000

    created validations for user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  validates :email, uniqueness: { case_sensitive: false}, presence:true 
+  validates :name, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
 end
